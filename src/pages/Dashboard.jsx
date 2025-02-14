79a351e682b7d88e72c58d07944febb889e8c94f
Sylvester Campbell <sylvester@imperiumapp.com>
2025-02-14 19:53:14 +0530

    changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -257,7 +257,7 @@ const Dashboard = () => {
   return (
     <>
       {localStorage.getItem("signIn") === "true" && (
-        <div className="relative h-svh bg-slate-950 ">
+        <div className="relative h-screen bg-slate-950 ">
           <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
 
           <div className="h-full w-full absolute overflow-y-auto scroll-smooth">
